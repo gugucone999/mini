f27e2e4e91ee982821586e32029ed3b96a62d0f1
gugucone999 <gugucone999@gmail.com>
2023-04-07 16:12:49 +0900

    project

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 terraform -chdir=/3tier/terraform/ apply -auto-approve
+
+sudo aws elasticache describe-cache-clusters \
+ --cache-cluster-id ec-project \
+ --show-cache-node-info > elasticache.json
+
 ansible-playbook -i /3tier/ansible/aws_rds.yaml -i /3tier/ansible/aws_ec2.yaml  /3tier/ansible/playbook --user ubuntu
