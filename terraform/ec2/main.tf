6a78fd63959db44193500fd8e0d6d7c773cd3f4d
gugucone999 <gugucone999@gmail.com>
2023-04-11 16:39:19 +0900

    project

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -4,20 +4,20 @@ module "module_vpc" {
 
 
 
-resource "aws_instance" "Redis_check" {
-  count                   = 1
-  ami           = var.my_server_ami
-  instance_type = var.my_server_type
+# resource "aws_instance" "Redis_check" {
+#   count                   = 1
+#   ami           = var.my_server_ami
+#   instance_type = var.my_server_type
 
-  vpc_security_group_ids = [ "${module.module_vpc.my_django_sg_id}" ]
-  subnet_id = "${module.module_vpc.my-vpc-project-subnet1_id}"
-  key_name = "project"
+#   vpc_security_group_ids = [ "${module.module_vpc.my_django_sg_id}" ]
+#   subnet_id = "${module.module_vpc.my-vpc-project-subnet1_id}"
+#   key_name = "project"
 
-  tags = {
-    Name  = "redis-${count.index}"
-    group = "redis"
-  }
-}
+#   tags = {
+#     Name  = "redis-check-${count.index}"
+#     group = "redis"
+#   }
+# }
 
 
 resource "aws_instance" "my_django" {
@@ -37,74 +37,74 @@ resource "aws_instance" "my_django" {
   }
 }
 
-resource "aws_lb" "django" {
-  name     = "my-lb"
-  security_groups = [ "${module.module_vpc.my_django_sg_id}" ]
-  load_balancer_type = "application"
-  subnets  = ["${module.module_vpc.my-vpc-project-subnet1_id}", "${module.module_vpc.my-vpc-project-subnet2_id}",
-              "${module.module_vpc.my-vpc-project-subnet3_id}", "${module.module_vpc.my-vpc-project-subnet4_id}"]
-  internal = false
+# resource "aws_lb" "django" {
+#   name     = "my-lb"
+#   security_groups = [ "${module.module_vpc.my_django_sg_id}" ]
+#   load_balancer_type = "application"
+#   subnets  = ["${module.module_vpc.my-vpc-project-subnet1_id}", "${module.module_vpc.my-vpc-project-subnet2_id}",
+#               "${module.module_vpc.my-vpc-project-subnet3_id}", "${module.module_vpc.my-vpc-project-subnet4_id}"]
+#   internal = false
 
-  tags = {
-    Name = "my-lb"
-  }
-}
+#   tags = {
+#     Name = "my-lb"
+#   }
+# }
 
-resource "aws_lb_target_group" "django" {
-  name     = "my-lb-target"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "${module.module_vpc.my-vpc-project_id}"
+# resource "aws_lb_target_group" "django" {
+#   name     = "my-lb-target"
+#   port     = 80
+#   protocol = "HTTP"
+#   vpc_id   = "${module.module_vpc.my-vpc-project_id}"
 
-  health_check {
-    interval            = 30
-    path                = "/"
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-  }
+#   health_check {
+#     interval            = 30
+#     path                = "/"
+#     healthy_threshold   = 3
+#     unhealthy_threshold = 3
+#   }
 
-  tags = {
-     Name = "django Target Group" 
-    }
-}
+#   tags = {
+#      Name = "django Target Group" 
+#     }
+# }
 
-resource "aws_lb_listener" "django" {
-  load_balancer_arn = aws_lb.django.arn
-  port              = "80"
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "django" {
+#   load_balancer_arn = aws_lb.django.arn
+#   port              = "80"
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.django.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.django.arn
+#   }
+# }
 
-resource "aws_lb_target_group_attachment" "django" {
-  count = 5
-  target_group_arn = aws_lb_target_group.django.arn
-  target_id        = element(aws_instance.my_django.*.id, count.index)
-  port             = 80
-}
+# resource "aws_lb_target_group_attachment" "django" {
+#   count = 5
+#   target_group_arn = aws_lb_target_group.django.arn
+#   target_id        = element(aws_instance.my_django.*.id, count.index)
+#   port             = 80
+# }
 
-resource "aws_elasticache_subnet_group" "ec_subnet_group" {
-  name       = "ec-subnet-group"
-  subnet_ids = ["${module.module_vpc.my-vpc-project-subnet1_id}", "${module.module_vpc.my-vpc-project-subnet2_id}",
-                "${module.module_vpc.my-vpc-project-subnet3_id}", "${module.module_vpc.my-vpc-project-subnet4_id}"]
-}
+# resource "aws_elasticache_subnet_group" "ec_subnet_group" {
+#   name       = "ec-subnet-group"
+#   subnet_ids = ["${module.module_vpc.my-vpc-project-subnet1_id}", "${module.module_vpc.my-vpc-project-subnet2_id}",
+#                 "${module.module_vpc.my-vpc-project-subnet3_id}", "${module.module_vpc.my-vpc-project-subnet4_id}"]
+# }
 
 # 단일 ec 만들때 사용!
 
-resource "aws_elasticache_cluster" "ec_cluster" {
-  cluster_id           = "ec-project"
-  engine               = "redis"
-  node_type            = "cache.t2.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis7"
-  engine_version       = "7.0"
-  port                 = 6379
-  subnet_group_name    = aws_elasticache_subnet_group.ec_subnet_group.name
-  security_group_ids   = [ "${module.module_vpc.my_redis_sg_id}" ]
-}
+# resource "aws_elasticache_cluster" "ec_cluster" {
+#   cluster_id           = "ec-project"
+#   engine               = "redis"
+#   node_type            = "cache.t2.micro"
+#   num_cache_nodes      = 1
+#   parameter_group_name = "default.redis7"
+#   engine_version       = "7.0"
+#   port                 = 6379
+#   subnet_group_name    = aws_elasticache_subnet_group.ec_subnet_group.name
+#   security_group_ids   = [ "${module.module_vpc.my_redis_sg_id}" ]
+# }
 
 # resource "aws_elasticache_replication_group" "ec_replication_group" {
 #   replication_group_id       =  "ec-replication-group"
