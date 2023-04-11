1ffebeba6aaa02691cd34c5e2291644d61a17459
gugucone999 <gugucone999@gmail.com>
2023-04-11 10:18:14 +0900

    project

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -77,12 +77,9 @@ resource "aws_lb_listener" "django" {
 
 
 resource "aws_lb_target_group_attachment" "django" {
-  for_each = aws_instance.my_django
-
   target_group_arn = aws_lb_target_group.django.arn
   target_id        = each.value.id
   port             = 80
-
   count = contains(each.value.tags["Name"], "my_django") ? 1 : 0
 }
 
