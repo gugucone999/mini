b5fde0e3b2709ecfa72b722ca8d35a57bf31d9a2
gugucone999 <gugucone999@gmail.com>
2023-04-11 15:09:28 +0900

    project

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -92,16 +92,30 @@ resource "aws_elasticache_subnet_group" "ec_subnet_group" {
                 "${module.module_vpc.my-vpc-project-subnet3_id}", "${module.module_vpc.my-vpc-project-subnet4_id}"]
 }
 
-resource "aws_elasticache_cluster" "ec_cluster" {
-  cluster_id           = "ec-project"
-  engine               = "redis"
-  node_type            = "cache.t2.micro"
-  num_cache_nodes      = 3
-  parameter_group_name = "default.redis7"
-  engine_version       = "7.0"
-  port                 = 6379
-  subnet_group_name    = aws_elasticache_subnet_group.ec_subnet_group.name
-  security_group_ids   = [ "${module.module_vpc.my_redis_sg_id}" ]
+# resource "aws_elasticache_cluster" "ec_cluster" {
+#   cluster_id           = "ec-project"
+#   engine               = "redis"
+#   node_type            = "cache.t2.micro"
+#   num_cache_nodes      = 1
+#   parameter_group_name = "default.redis7"
+#   engine_version       = "7.0"
+#   port                 = 6379
+#   subnet_group_name    = aws_elasticache_subnet_group.ec_subnet_group.name
+#   security_group_ids   = [ "${module.module_vpc.my_redis_sg_id}" ]
+# }
+
+resource "aws_elasticache_replication_group" "ec_replication_group" {
+  replication_group_id       = "ec-project"
+  replication_group_description = "ElastiCache Redis Replication Group"
+  node_type                  = "cache.t2.micro"
+  number_cache_clusters      = 2
+  automatic_failover_enabled = true
+  subnet_group_name          = aws_elasticache_subnet_group.ec_subnet_group.name
+  parameter_group_name       = "default.redis7"
+  port                       = 6379
+  security_group_ids         =  [ "${module.module_vpc.my_redis_sg_id}" ]
+  engine_version             = "6.x"
+  engine                     = "redis"
 }
 
 resource "aws_db_subnet_group" "my_db_subnet_group" {
