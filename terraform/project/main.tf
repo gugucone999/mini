6e0da86e4789caa354efc370450f80d832575cc3
gugucone999 <gugucone999@gmail.com>
2023-04-12 15:58:54 +0900

    project

--- terraform/project/main.tf
+++ terraform/project/main.tf
@@ -118,6 +118,11 @@ resource "aws_elasticache_replication_group" "ec_replication_group" {
   security_group_ids         =  [ "${module.module_vpc.my_redis_sg_id}" ]
   engine_version             = "7.0"
   engine                     = "redis"
+  timeouts = {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
 }
 
 resource "aws_db_subnet_group" "my_db_subnet_group" {
@@ -141,7 +146,7 @@ resource "aws_db_instance" "my-master" {
   skip_final_snapshot  = true
   multi_az = false
   backup_retention_period = 7
-  timeouts {
+  timeouts = {
     create = "30m"
     update = "30m"
     delete = "30m"
@@ -158,7 +163,7 @@ resource "aws_db_instance" "my-read-replica" {
   skip_final_snapshot  = true
   multi_az = false
   backup_retention_period = 7
-  timeouts {
+  timeouts = {
     create = "30m"
     update = "30m"
     delete = "30m"
