29c28422a367a574e82402ff595e59f5942bdbf7
gugucone999 <gugucone999@gmail.com>
2023-04-12 15:56:04 +0900

    project

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,12 +8,14 @@ module "module_project" {
   source = "./project"
   my_server_ami = "ami-0c6e5afdd23291f73"
   my_server_type = "t2.micro"
-  timeouts {
+  timeouts = {
     create = "30m"
     update = "30m"
     delete = "30m"
   }
 }
+
+
 
 # module "module_rds" {
 #   source = "./rds"
