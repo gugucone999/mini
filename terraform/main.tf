6d6a5c2ee7b3ba706c11052cb7826e8e43ca43ae
gugucone999 <gugucone999@gmail.com>
2023-04-13 08:56:54 +0900

    project

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,11 @@ provider "aws" {
   region  = "ap-northeast-2"
 }
 
-
-
 module "module_project" {
   source = "./project"
   my_server_ami = "ami-0c6e5afdd23291f73"
   my_server_type = "t2.micro"
 }
-
-
 
 # module "module_rds" {
 #   source = "./rds"
@@ -23,4 +19,3 @@ module "module_project" {
 # module "module_vpc" {
 #   source = "./vpc"
 # }
-
