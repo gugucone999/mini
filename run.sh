48410487a4aff458f4a552fbd46d773a41dae377
gugucone999 <gugucone999@gmail.com>
2023-04-13 14:44:28 +0900

    project

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-terraform -chdir=project/terraform/ init
+# terraform -chdir=project/terraform/ init
 
-export TF_CLI_TIMEOUT=0
+# export TF_CLI_TIMEOUT=0
 
-terraform -chdir=project/terraform/ apply -auto-approve -parallelism=10
+# terraform -chdir=project/terraform/ apply -auto-approve -parallelism=10
 
 # sudo aws elasticache describe-cache-clusters \
 #  --cache-cluster-id ec-project \
@@ -13,4 +13,4 @@ terraform -chdir=project/terraform/ apply -auto-approve -parallelism=10
  aws elasticache describe-replication-groups \
  --replication-group-id ec-replication-group > /home/ubuntu/elasticache.json
 
-# ansible-playbook -i project/ansible/aws_rds.yaml -i project/ansible/aws_ec2.yaml  project/ansible/playbook --user ubuntu
+ansible-playbook -i project/ansible/aws_rds.yaml -i project/ansible/aws_ec2.yaml  project/ansible/playbook --user ubuntu
